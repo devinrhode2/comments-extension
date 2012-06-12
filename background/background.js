2a146e2e5f1bab080cfb4c7b041ceb4d9a0dc9e0
Devin Rhode <devinrhode2@gmail.com>
2012-06-12 13:58:52 -0700

    boom!

---
+++ background/background.js
@@ -0,0 +1,146 @@
+'use strict';
+log(appName + ' background.js');
+
+if (localStorage.getItem('installed') !== 'installed') {
+  localStorage.setItem('installed', 'installed');
+  chrome.tabs.create({
+    'url': 'http://devinrhode2.com/fbcomments/welcome.html'
+  });
+}
+
+//if some tab loads and the url has '?fb_comment_id=fbc_' in it, load the comments sidebar
+chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
+  if (details.url.has('?fb_comment_id=fbc_')) {
+    browserActionOnClicked({
+      url: details.url,
+      id: details.tabId
+    });
+  }
+});
+
+window.needUpdate = false;
+GET('../manifest.json', function manifestGET(manifest){
+  manifest = JSON.parse(manifest);
+  var version = manifest.version;
+  GET('http://thescoutapp.com/extension/fb-comments-sidebar-version.json', function versionCheckCallback(version){
+    if (parseFloat(version) > version) {
+      window.needUpdate = true;
+    } else {
+      window.needUpdate = false;
+      log('up to date!');      
+    }
+  });
+});
+
+var browserActionOnClicked = function browserActionOnClicked(tab) {
+  if (!tab.url) {
+    return 'falsy url';
+  }
+  if (false/*tab.url.indexOf('://') !== 0*/) {
+    return warn('non-webpage tab sent to browserActionOnClicked');
+  } else {
+    if (tab.url.has('://chrome.google.com/webstore')) {
+      if (confirm('Google blocks extensions from working on the WebStore. Press OK to send a complaint to Google.')) {
+        chrome.tabs.create({
+          'url': 'http://support.google.com/chrome_webstore/bin/request.py?contact_type=cws_user_contact'
+        });
+      }
+    } else {
+      if (needUpdate) {
+        chrome.tabs.executeScript(tab.id, {'code': 'alert("Press Ok to update facebook comments sidebar");'}, function needUpdateCallback(){
+          chrome.tabs.create({
+            'url': 'https://chrome.google.com/webstore/detail/...'
+          });
+        });
+      }
+      var tabLabel = 'execution time for: ' + tab.url;
+      console.time(tabLabel);
+      chrome.tabs.executeScript(tab.id, {'file': 'include.js'}, function(){
+        chrome.tabs.executeScript(tab.id, {'file': 'inject.js'}, function(){
+          console.timeEnd(tabLabel);
+        });
+      });
+    }
+  }
+};
+chrome.browserAction.onClicked.addListener(browserActionOnClicked);
+
+window.cache = {};
+
+var updateCommentCount = function updateCommentCount(number) {
+  if (number === 0) {
+    chrome.browserAction.setBadgeBackgroundColor({'color': '#D2D9E7'});//<--dull gray. good black: #666
+  } else {
+    chrome.browserAction.setBadgeBackgroundColor({'color': '#C45D4F'});//<--that's a 50% saturation of the fb red: #F03D25
+  }
+  chrome.browserAction.setBadgeText({'text': number.toString()});
+};
+
+var newTab = function newTab(tabId, url) {
+  //if in cache, return immediately!
+  if (!url) {
+    return warn('falsy url');
+  }
+  
+  //clean url...
+  url = cleanUrl(url);
+  
+  if (typeof cache[url] !== 'undefined') {
+    updateCommentCount(cache[url].comments);
+  } else {
+    if (url.indexOf('http') !== 0) {
+      updateCommentCount(0);
+      return warn('newTab non-webpage: ' + url);
+    }
+    GET('https://graph.facebook.com/?ids=' + url, function CommentsGetCallback(resp){
+      var resp = JSON.parse(resp);
+      log('fb json for: '+url+':\n', resp);
+      if (!resp[url]) {
+        var respKeys = Object.keys(resp);
+        if (respKeys) {
+          url = respKeys[0];
+          if (respKeys.length !== 1) {
+            if (respKeys.length !== 0) {
+            
+            } else {
+              //alert('facebook gave us multi key JSON:' + JSON.stringify(respKeys));
+              console.error(
+                '*** FACEBOOK RETURNED MULTI-KEY JSON, CONDITION IS NOT EXPECTED OR HANDLED ***\n',
+                respKeys,
+                '\n' + respKeys.length
+              );
+            }
+          }
+        }
+      }
+      cache[url] = resp[url];
+      if (cache[url] && !cache[url].comments) {
+        cache[url].comments = 0;
+      }
+      if (!cache[url].comments) {
+        cache[url].comments = 0;
+      }
+      updateCommentCount(cache[url].comments);
+    });
+  }
+};
+
+//update comment count on tabs
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo && typeof changeInfo.url !== 'undefined' && changeInfo.url.length > 0) {
+    newTab(tab.id, changeInfo.url);
+  }
+});
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function(tab){
+    newTab(tab.id, tab.url);
+  });
+});
+
+//update comment count on current active tabs. only relevant when developing or when an update occurs.
+chrome.tabs.query({active: true}, function queryCallback(tabs){
+  var length = tabs.length;
+  for (var i = 0; i < length; i++) {
+    newTab(tabs[i].id, tabs[i].url);
+  }
+});
---
+++ inject.js
@@ -0,0 +1,83 @@
+(function injectJSStrict(){
+'use strict';
+log(appName + ' inject.js');
+
+if (typeof shown === 'undefined') {
+  //this if branch only executes once. Yes, shown is still defined over multiple script injections
+  window.shown = true;
+  window.html = document.documentElement;
+  window.body = document.body;
+  
+  window.adjustHtmlMargin = function addHtmlMargin(adjustment){
+    var currentRight = parseFloat(getComputedStyle(html).marginRight);
+    html.style.marginRight = (currentRight + adjustment + 20) + 'px';
+  };
+  
+  //450 width because things start overflowing poorly at smaller widths...
+  //facebook designed this at a 450px width
+  window.width = 450;
+  
+  //used to flip from display: none and display: block
+  window.styleFlip = $.createElement('style', {
+    innerHTML: '#' + appName + ' { display: block; } '
+  });
+  html.appendChild(styleFlip);
+  
+  var cleanedUrl = cleanUrl(location.href);
+  
+  adjustHtmlMargin(width);
+  var comments = $.createElement('div', {
+    className: 'fb-comments',
+    id: appName
+   }, {
+    style: '\
+      overflow: auto;\
+      padding: 10px;\
+      white-space: nowrap;\
+      position:fixed;\
+      right: 0px;\
+      top: 0px;\
+      z-index:2147483647;\
+      -webkit-user-select: none;\
+      height: 100%;\
+      width: '+width+'px;\
+      background: whiteSmoke;\
+      border-left: 1px solid #6F6F6F;\
+      -webkit-box-shadow: -2px 0px 5px 0px rgba(128, 128, 128, 0.6);\
+      margin-top: -1px;',
+    'data-href': cleanedUrl,
+    //'data-mobile': 'true', ... doesn't seem to fluidly move to less than 450px... facebook...
+    'data-num-posts': '13',
+    'data-width':     width
+  });
+  body.appendChild(comments);
+  
+  var fbRoot = $.id('fb-root');
+  if (!fbRoot) {
+    body.appendChild($.createElement('div', {id: 'fb-root'}));
+  }
+  //BOOM
+  html.appendChild(
+    $.createElement('script', {
+      src: 'https://connect.facebook.net/en_US/all.js#xfbml=1&appId=246571968740345',
+      id: 'facebook-jssdk'
+    })
+  );
+  html.appendChild($.createElement('script',{
+    innerHTML:'if (typeof FB !== "undefined"){FB.XFBML.parse();}console.log("XFBML.parse\'d");'
+  }));
+} else {//comments already initialized
+  if (shown) {
+    //hide
+    adjustHtmlMargin(-1 * width);//negative, undo width adjustment
+    shown = false;
+    styleFlip.innerHTML = '#' + appName + ' { display: none; } ';
+  } else {
+    //show back
+    adjustHtmlMargin(width);
+    shown = true;
+    styleFlip.innerHTML = '#' + appName + ' { display: block; } ';    
+  }
+}
+
+}());
